12a0125e114fab109d9a172df305a28512d6c4f1
19840769 <kodikarah.21@uom.lk>
2024-10-06 00:10:53 +0530

    updated

--- server.js
+++ server.js
@@ -2,21 +2,21 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import { connectDB } from "./config/db.js";
-//import videoRoutes from "./routes/video.js";
+import videoRoutes from "./routes/video.js";
 //import { errorHandler } from "./middlewares/error.js";
 
 dotenv.config();
 
 // Express App
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 2001;
 
 // Middlewares
 app.use(cors());
 app.use(express.json());
 
 // Routes
-//app.use("/api/videos", videoRoutes);
+app.use("/api/videos", videoRoutes);
 
 //app.use(errorHandler);
 
