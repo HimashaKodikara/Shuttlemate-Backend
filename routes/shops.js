f9539e86248b987a633b8b75c5b22e8c9ad26c5e
19840769 <kodikarah.21@uom.lk>
2025-02-16 23:26:07 +0530

    (change)

--- routes/shops.js
+++ routes/shops.js
@@ -1,7 +1,10 @@
 import express from "express";
 import { 
   createShop, 
-  getAllShops, 
+  getAllShops,
+  deleteShop ,
+  addItemToShop,
+  removeItemFromShop,
  
 } from "../controllers/shopController.js";
 
@@ -10,6 +13,13 @@ const router = express.Router();
 // http://localhost:5000/api/shops/
 router.post("/", createShop);
 router.get("/", getAllShops);
+router.delete("/shop/:id",deleteShop);
+//  http://localhost:5000/api/shops/:id/items/
+router.post("/:id/items",addItemToShop);
+// http://localhost:5000/api/shops/shop/:id/items/:id
+router.delete("/shop/:id/items/:itemId", removeItemFromShop);
+
+
 
 
 // Define item routes within shops
